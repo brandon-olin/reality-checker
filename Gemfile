a623263c70cb35852e9b1a988ceef24f4fe11d42
brandon olin <olin.brandon@gmail.com>
2020-04-22 13:22:55 -0400

    saving progress for end of day

--- Gemfile
+++ Gemfile
@@ -30,6 +30,7 @@ gem 'bootsnap', '>= 1.4.2', require: false
 
 gem 'autoprefixer-rails'
 gem 'simple_form'
+gem 'yt', '~> 0.32.5'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/controllers/politician_controller.rb
+++ app/controllers/politician_controller.rb
@@ -1,5 +1,13 @@
 class PoliticianController < ApplicationController
-  def trump
+  before_action yt_configure, only: [:trump]
 
+  def trump
+    # @youtube = Yt::Playlist.new(id: 'PLRJNAhZxtqH-fd1x12yYa-hKuxoZ8Npnz').playlist_items
+  end
+
+  def yt_configure
+    # Yt.configure do |config|
+    #   config.api_key = 'AIzaSyCc-NMroCyISYoj6VUNPmO3qvctu2jrCc0'
+    # end
   end
 end
