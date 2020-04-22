b68400281d6a48633330b67b07557f73efcf1dc4
brandon olin <olin.brandon@gmail.com>
2020-04-22 10:35:45 -0400

    content added to home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get 'trump', to: 'politician#trump'
+  get 'about', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
